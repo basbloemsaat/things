13d7c7cbe063cc40fd20a0d600112b9a50c5c715
Bas Bloemsaat <bas@bloemsaat.com>
2020-04-11 12:33:39 +0200

    changes to base

--- docs/covid19_chart/modules/chart.js
+++ docs/covid19_chart/modules/chart.js
@@ -1,7 +1,7 @@
 import 'https://d3js.org/d3.v5.js';
 
 let config = {
-    margin: 10,
+    padding: 10,
 }
 
 class Chart {
@@ -15,7 +15,7 @@ class Chart {
         this.y_axis_g = g.append('g').classed('yaxis', true);
 
 
-        // temp scales, used for sizing
+        // default scales, used for sizing
         this.x = d3.scaleTime()
             .domain([new Date(2019, 11, 31), new Date()])
             .range([0, 100]);
@@ -23,7 +23,7 @@ class Chart {
         this.x_axis_g.call(this.x_axis);
 
         this.y = d3.scaleLinear()
-            .domain(0, 10000)
+            .domain([1000, 0])
             .range([0, 100]);
         this.y_axis = d3.axisLeft(this.y)
         this.y_axis_g.call(this.y_axis);
@@ -32,23 +32,24 @@ class Chart {
     }
 
     reposistion_elements() {
+        console.log('xyz')
         let x_height = this.x_axis_g.node().getBBox()['height'];
         let y_width = this.y_axis_g.node().getBBox()['width'];
 
         let svg_height = this.svg.node().clientHeight;
         let svg_width = this.svg.node().clientWidth;
 
-        let canvas_height = svg_height - x_height;
-        let canvas_width = svg_width - y_width;
+        let canvas_height = svg_height - x_height - (2 * config.padding);
+        let canvas_width = svg_width - y_width - (2 * config.padding);
 
 
         this.x_axis_g
-            .attr('transform', 'translate(' + y_width + ',' + canvas_height + ')')
+            .attr('transform', 'translate(' + (y_width+config.padding) + ',' + (canvas_height + config.padding) + ')')
         this.y_axis_g
-            .attr('transform', 'translate(' + y_width + ',0)')
+            .attr('transform', 'translate(' + (y_width+config.padding) + ',' + config.padding + ')')
 
         this.canvas
-            .attr('transform', 'translate(' + y_width + ',0)')
+            .attr('transform', 'translate(' + (y_width+config.padding) + ',' + config.padding + ')')
 
         this.x.range([0, canvas_width]);
         this.x_axis_g.call(this.x_axis);
--- docs/covid19_chart/modules/main.js
+++ docs/covid19_chart/modules/main.js
@@ -5,6 +5,34 @@ import { Chart } from './chart.js';
 
 let svg = d3.select('svg#chart');
 
-svg.attr('height', window.innerHeight - Math.ceil(d3.select('div#chart_options').node().offsetHeight) -10)
-
 var chart = new Chart(svg);
+
+let data = {
+    confirmed: {},
+}
+
+Promise.all([
+    d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"),
+    d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"),
+    d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"),
+]).then(function(files) {
+    data.confirmed = files[0];
+    data.deaths = files[1];
+    data.recovered = files[2];
+
+    console.log(data);
+}).catch(function(err) {
+    // handle error here
+    console.log(err)
+})
+
+
+
+
+let redraw = function() {
+    svg.attr('height', window.innerHeight - Math.ceil(d3.select('div#chart_options').node().offsetHeight) - 10)
+    chart.reposistion_elements();
+}
+
+window.addEventListener("resize", redraw);
+redraw();
